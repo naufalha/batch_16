b943f4f8aa9d18b28021446665027fd034a9f87e
naufalha <naufalha48@gmail.com>
2025-11-05 11:58:05 +0700

    foobar

---
+++ Foobar/Program.cs
@@ -0,0 +1,32 @@
+﻿public class FooBar
+{
+    public static void Main(string[] args)
+    {
+        int n = 30;
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 15 == 0)
+            {
+                Console.Write("foobar");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.Write("foo");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.Write("bar");
+            }
+            else
+            {
+                Console.Write(i);
+            }
+
+            if (i < n)
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
