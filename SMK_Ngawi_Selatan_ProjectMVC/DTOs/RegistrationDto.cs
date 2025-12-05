f4e5d8bdd5218547a559b532dfc9322d821bafdd
naufalha <naufalha48@gmail.com>
2025-12-05 15:03:46 +0700

    hasta la vista baby

--- SMK_Ngawi_Selatan_ProjectMVC/DTOs/RegistrationDto.cs
+++ SMK_Ngawi_Selatan_ProjectMVC/DTOs/RegistrationDto.cs
@@ -5,6 +5,8 @@ namespace SmkNgawi.DTOs
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public int TargetClassRoomId { get; set; } // User memilih ID Kelas (Misal: ID 1 untuk X-TKJ-1)
+
+        
         public DateTime RegistrationDate { get; set; }
     
     }
